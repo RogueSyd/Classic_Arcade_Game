1509a6986911bdeb64d8ab5ffcf168b4978292b7
RogueSyd <thayne.syd@gmail.com>
2023-03-06 16:28:44 -0500

    Update Main.java
    Added some documentation.

--- Main.java
+++ Main.java
@@ -6,12 +6,16 @@ John Borra, Sarah Drury, Oanh Woodworth, Yeng Veng, and Thayne Emery
  */
 
 import java.awt.EventQueue;
+/**
+ * Main class.
+ */
 public class Main 
 {
      public static void main(String[] args) 
      {
         EventQueue.invokeLater(() -> 
         {
+        	// Creates a Pac-Man object and sets it to visible.
             var ex = new Pacman();
             ex.setVisible(true);
         });
